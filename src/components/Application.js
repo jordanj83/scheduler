c2ef3ebdceccf08f1ed857f178411e74df4184fa
jordanj83 <jordan.johnson@telus.com>
2023-01-13 12:49:00 -0800

    retrieving interviewer data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,24 +1,22 @@
-import React , {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
-import Appointment from "./Appointment";
+import Appointment from "components/Appointment";
 import axios from "axios";
 import { getAppointmentsForDay } from "helpers/selectors";
-
-
+import { getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
+  const setDay = day => setState(prev => ({ ...prev, day }));
+  // const setDays = days => setState(prev => ({ ...prev, days }));
 
-
-  const setDay = day => setState(prev => ({...prev, day }))
-  // const setDays = days => setState(prev => ({ ...prev, days }))
 
   useEffect(() => {
     Promise.all([
@@ -26,47 +24,61 @@ export default function Application(props) {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ]).then((all) => {
-      setState(prev => ({
-        ...prev, days: all[0].data, 
-        appointments: all[1].data}))
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
-    }, []); 
 
-    const dailyAppointments = getAppointmentsForDay(state, state.day)
+  }, []);
 
-    const appointmentsArr = dailyAppointments.map(appointment => {
-        return (
-          <Appointment
-            key={appointment.id}
-            {...appointment}
-          />
-        );
-      })
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map((appointment) => {
+  const interview = getInterview(state, appointment.interview);
+
+  return (
+    <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
+    />
+  );
+});
+
+
+  const appointmentsArray = dailyAppointments.map(appointment => {
+    return (
+      <Appointment key={appointment.id}
+        {...appointment} />
+    );
+  });
+
 
   return (
     <main className="layout">
       <section className="sidebar">
         <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList 
-  days={state.days} 
-  value={state.day} 
-  onChange={setDay} 
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={state.days}
+            value={state.day}
+            onChange={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {appointmentsArr}
+        {appointmentsArray}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,3 +13,20 @@ const output = filteredDays[0].appointments.map(appointment => {
 
 return output
 }
+
+
+
+export function getInterview(state, interview) {
+
+  const { interviewers } = state;
+
+  if (!interview) {
+    return null;
+  }
+
+  const interviewerId = interview.interviewer;
+  const interviewer = interviewers[interviewerId];
+
+  return {...interview, interviewer}
+
+}
